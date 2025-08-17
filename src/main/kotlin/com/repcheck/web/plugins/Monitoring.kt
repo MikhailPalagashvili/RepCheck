b5f6a83533f25f108c94e99f9e5f439f48bd4c55
mikhailpalagashvili <mikhailpala@gmail.com>
2025-08-17 16:51:40 -0400

    chore: refactor and remove

--- src/main/kotlin/com/repcheck/web/plugins/Monitoring.kt
+++ src/main/kotlin/com/repcheck/web/plugins/Monitoring.kt
@@ -4,7 +4,7 @@ import io.ktor.server.application.*
 import io.ktor.server.plugins.callid.*
 import io.ktor.server.plugins.callloging.*
 import io.ktor.server.request.*
-import java.util.UUID
+import java.util.*
 
 fun Application.installMonitoring() {
     install(CallId) {
